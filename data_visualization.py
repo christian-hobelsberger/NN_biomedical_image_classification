3213dac5067279efd63bf5884ce55f21c818ed36
Christian Hobelsberger <chobelsberger@gmail.com>
2024-05-08 09:38:49 +0200

    some updates to data viz

--- data_visualization.py
+++ data_visualization.py
@@ -69,26 +69,90 @@ print("Table of Label Counts:")
 print(df_label_counts)
 
 # Plot the distribution of labels
-plt.figure(figsize=(16, 8))
+plt.figure(figsize=(12, 6))
 sns.countplot(y='Label', data=df_labels, palette='pastel')
 # plt.title('Distribution of Labels in Training Set', fontsize=20)
 plt.xlabel('Count', fontsize=16)
-plt.ylabel('Label', fontsize=16)
+plt.ylabel('Cell Type', fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=12)
 plt.tight_layout()
 plt.savefig("plots/train_label_distribution.png")
 
+# 3. Visualize the distribution of the labels of all splits
 
-# 3. Visualize the first three images for each label category
+# Function to create DataFrame and label counts
+def create_label_df(data, split_name):
+    # Create DataFrame with labels
+    df_labels = pd.DataFrame(data.labels, columns=["Label"])
+
+    # Replace label codes with their full names
+    df_labels["Label"] = df_labels["Label"].map(label_names)
+
+    # Calculate the count of each label category and sort by index
+    label_counts = df_labels["Label"].value_counts().sort_index()
+
+    # Create a DataFrame directly from label counts
+    df_label_counts = pd.DataFrame({"Label": label_counts.index, "Count": label_counts.values})
+
+    # Add split information
+    df_label_counts['Split'] = split_name
+    
+    return df_label_counts
+
+# Create label counts DataFrame for training set
+df_train_label_counts = create_label_df(data_blood_train, 'Train')
+
+# Create label counts DataFrame for validation set
+df_val_label_counts = create_label_df(data_blood_val, 'Validation')
+
+# Create label counts DataFrame for test set
+df_test_label_counts = create_label_df(data_blood_test, 'Test')
+
+# Concatenate label counts DataFrames
+df_label_counts = pd.concat([df_train_label_counts, df_val_label_counts, df_test_label_counts])
+
+# Reset index
+df_label_counts.reset_index(drop=True, inplace=True)
+
+
+# Calculate total count for each split
+total_count_train = df_label_counts[df_label_counts['Split'] == 'Train']['Count'].sum()
+total_count_val = df_label_counts[df_label_counts['Split'] == 'Validation']['Count'].sum()
+total_count_test = df_label_counts[df_label_counts['Split'] == 'Test']['Count'].sum()
+
+# Calculate relative frequencies for each split
+df_label_counts['Relative Frequency'] = df_label_counts.apply(lambda row: row['Count'] / total_count_train if row['Split'] == 'Train' else (row['Count'] / total_count_val if row['Split'] == 'Validation' else row['Count'] / total_count_test), axis=1)
+
+# Set the figure size
+plt.figure(figsize=(12, 6))
+
+# Create a shifted barplot with axis flipped
+sns.barplot(x='Relative Frequency', y='Label', hue='Split', data=df_label_counts, palette='pastel')
+
+# Set the title and labels
+plt.title('Relative Distribution of Cell Types by Split', fontsize=16)
+plt.xlabel('Relative Frequency', fontsize=14)
+plt.ylabel('Cell Type', fontsize=14)
+
+# Add a legend
+plt.legend(title='Split', fontsize=12)
+
+# Show plot
+plt.tight_layout()
+plt.savefig("plots/split_label_distribution.png")
+plt.show()
+
+
+# 4. Visualize the first three images for each label category
 images_array = data_blood_train.imgs
 labels_array = data_blood_train.labels.flatten()
 
 # Create a figure with subplots
-fig, axes = plt.subplots(nrows=5, ncols=len(label_names), figsize=(15, 10))
+fig, axes = plt.subplots(nrows=3, ncols=len(label_names), figsize=(16, 6))
 
 # Loop through each category and corresponding images
 for i, label in enumerate(label_names.keys()):
-    label_images = images_array[labels_array == label][:5]  # Select first three images for the category
+    label_images = images_array[labels_array == label][:3]  # Select first three images for the category
     for j, image in enumerate(label_images):
         # Plot each image
         axes[j, i].imshow(np.array(image))
